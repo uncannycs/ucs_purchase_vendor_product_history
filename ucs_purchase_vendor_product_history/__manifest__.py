c438cca64f348bf91ca47c9d3e68461f94efd233
uncannycs <uncannycs@gmail.com>
2026-07-17 18:45:33 +0530

    [MOD] cm-1 update index & manifest file

--- ucs_purchase_vendor_product_history/__manifest__.py
+++ ucs_purchase_vendor_product_history/__manifest__.py
@@ -5,21 +5,22 @@
     'summary': """User can view The Purchase history of The 
     products from Purchase Order Line""",
     'description': """Purchases history of products from Purchase Order Line""",
-    'author': "Cybrosys Techno Solutions",
-    'company': "Cybrosys Techno Solutions",
-    'maintainer': 'Cybrosys Techno Solutions',
-    'website': "https://www.cybrosys.com",
+    'author': "Uncanny Consulting Services LLP",
+    'company': "Uncanny Consulting Services LLP",
+    'maintainer': 'Uncanny Consulting Services LLP',
+    'website': "https://www.uncannycs.com",
     'category': 'Purchase/Purchase',
-    'depends': ['base','purchase'],
-    'license': 'AGPL-3',
+    'depends': ['base', 'purchase'],
     'data': [
         'security/ir.model.access.csv',
         'wizard/product_purchase_order_history_wizard_views.xml',
         'views/purchase_order_view.xml',
     ],
     'images': ['static/description/banner.gif'],
+    'license': 'AGPL-3',
     'installable': True,
     'auto_install': False,
     "price": 25,
+    "currency": "USD"
 }
 
